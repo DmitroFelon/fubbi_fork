d15b6a723132ca08ef2b5d74921ea6f55e2e1b04
ImadBazzal <imad.bazzal.93@gmail.com>
2017-11-03 12:41:36 +0200

    user seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'imad.bazzal.93@gmail.com',
+            'password' => Hash::make('8734969091'),
+        ]);
+    }
+}
