158aedb61fabba17df2a82a7adcf30dcd820720c
ImadBazzal <imad.bazzal.93@gmail.com>
2017-11-07 11:45:35 +0200

    general events observers

---
+++ app/Observers/ClientObserver.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imad
+ * Date: 07/11/17
+ * Time: 11:44
+ */
+
+namespace App\Observers;
+
+use App\Models\Users\Client;
+
+class ClientObserver
+{
+    public function created(Client $client)
+    {
+        
+    }
+}
---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+
+class ProjectObserver
+{
+    public function created(Project $project)
+    {
+
+    }
+}
