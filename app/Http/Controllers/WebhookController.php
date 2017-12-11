7c50abf232ae988d5e75dd3b97e02d7e0f8d84f4
ImadBazzal <imad.bazzal.93@gmail.com>
2017-12-11 10:51:26 +0200

    test webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Response;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
 
 class WebhookController extends CashierController
@@ -11,6 +12,7 @@ class WebhookController extends CashierController
 	public function handleInvoicePaymentSucceeded($payload)
 	{
 		Log::debug(\GuzzleHttp\json_encode($payload));
+		return Response::json(['status' => 'recieved']);
 	}
 
 	public function handleInvoiceUpcoming($payload)
