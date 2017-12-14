704494c979a2e13019da7fff256f6eb7169b3b63
ImadBazzal <imad.bazzal.93@gmail.com>
2017-12-14 20:04:59 +0200

    KeywordTool integration

--- resources/views/entity/project/partials/form/keywords-step.blade.php
+++ resources/views/entity/project/partials/form/keywords-step.blade.php
@@ -1,7 +1,7 @@
 @foreach($keywords as $keyword)
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
         <label>
-            <input {{($keyword['accepted'])?'checked':''}} name="keywords[{{$keyword['theme']}}]" type="checkbox" value="{{$keyword['text']}}">
+            <input {{($keyword['accepted'])?'checked':''}} name="keywords[{{$keyword['theme']}}][]" type="checkbox" value="{{$keyword['text']}}">
             {{ucfirst($keyword['text'])}}
         </label>
     </div>
