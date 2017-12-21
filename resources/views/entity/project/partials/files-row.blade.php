8e02371dc65e8d81f1588803cb1d8db72c349c80
Imad <imad.bazzal.93@gmail.com>
2017-12-21 10:50:40 +0200

    export project data to zip and attach to notification email

--- resources/views/entity/project/partials/files-row.blade.php
+++ resources/views/entity/project/partials/files-row.blade.php
@@ -1,29 +1,26 @@
-
-<div class="col col-xs-3">
-    <div class="file-box">
-        <div class="file">
-            <span class="corner"></span>
-            <div class="icon">
-                @if($media->mime_type == 'image/jpeg' or $media->mime_type == 'image/png')
-                    <a href="{{$media->getFullUrl()}}"
-                       target="_blank"
-                       data-gallery="{{$media->collection_name}}">
-                        <img class="blueimp-gallery-image"
-                             src="{{$media->getFullUrl()}}">
-                    </a>
-                @else
-                    <a target="_blank" href="{{$media->getFullUrl()}}">
-                        <i class="fa fa-file"></i>
-                    </a>
-                @endif
-            </div>
-            <div class="file-name">
-                <a target="_blank" href="{{$media->getFullUrl()}}">
-                    {{$media->file_name}}
+<div class="file-box">
+    <div class="file">
+        <span class="corner"></span>
+        <div class="icon">
+            @if($media->mime_type == 'image/jpeg' or $media->mime_type == 'image/png')
+                <a href="{{$media->getFullUrl()}}"
+                   target="_blank"
+                   data-gallery="{{$media->collection_name}}">
+                    <img class="blueimp-gallery-image"
+                         src="{{$media->getFullUrl()}}">
                 </a>
-                <br/>
-                <small>Added: {{$media->created_at->format('Y-m-d')}}</small>
-            </div>
+            @else
+                <a target="_blank" href="{{$media->getFullUrl()}}">
+                    <i class="fa fa-file"></i>
+                </a>
+            @endif
+        </div>
+        <div class="file-name">
+            <a target="_blank" href="{{$media->getFullUrl()}}">
+                {{$media->file_name}}
+            </a>
+            <br/>
+            <small>Added: {{$media->created_at->format('Y-m-d')}}</small>
         </div>
     </div>
 </div>
