a5f6540cb2c1da070563e50b7afa7b5dfc3ad6de
Imad <imad.bazzal.93@gmail.com>
2017-12-21 09:52:10 +0200

    export project data to zip and attach to notification email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ use App\Observers\ProjectObserver;
 use App\Observers\UserObserver;
 use App\Services\Api\Keywords\KeywordsFactoryInterface;
 use App\Services\Api\Keywords\LocalKeywords;
+use App\Services\Api\KeywordTool;
 use App\User;
 use Form;
 use Illuminate\Support\Facades\Schema;
@@ -21,66 +22,66 @@ use Illuminate\Support\ServiceProvider;
  */
 class AppServiceProvider extends ServiceProvider
 {
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		Schema::defaultStringLength(191);
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
 
-		\Stripe\Stripe::setApiKey(config('services.stripe.secret'));
+        \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
 
-		$this->observers();
+        $this->observers();
 
-		$this->formComponents();
-	}
+        $this->formComponents();
+    }
 
-	private function observers()
-	{
-		Project::observe(ProjectObserver::class);
-		Invite::observe(InviteObserver::class);
-		User::observe(UserObserver::class);
-	}
+    private function observers()
+    {
+        Project::observe(ProjectObserver::class);
+        Invite::observe(InviteObserver::class);
+        User::observe(UserObserver::class);
+    }
 
-	private function formComponents()
-	{
-		Form::component(
-			'bsText',
-			'components.form.text',
-			[
-				'name',
-				'value',
-				'label',
-				'description',
-				'attributes' => [],
-				'type'       => '',
-			]
-		);
+    private function formComponents()
+    {
+        Form::component(
+            'bsText',
+            'components.form.text',
+            [
+                'name',
+                'value',
+                'label',
+                'description',
+                'attributes' => [],
+                'type' => '',
+            ]
+        );
 
-		Form::component(
-			'bsSelect',
-			'components.form.select',
-			[
-				'name',
-				'list',
-				'selected',
-				'label',
-				'description',
-				'select_attributes'  => [],
-				'options_attributes' => [],
-			]
-		);
-	}
+        Form::component(
+            'bsSelect',
+            'components.form.select',
+            [
+                'name',
+                'list',
+                'selected',
+                'label',
+                'description',
+                'select_attributes' => [],
+                'options_attributes' => [],
+            ]
+        );
+    }
 
-	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->bind(KeywordsFactoryInterface::class, LocalKeywords::class);
-	}
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(KeywordsFactoryInterface::class, KeywordTool::class);
+    }
 }
