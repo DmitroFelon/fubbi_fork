ebcfa5cd1549c08df4e1653045606b618820a41b
Imad <imad.bazzal.93@gmail.com>
2017-12-21 09:46:46 +0200

    export project data to zip and attach to notification email

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -6,24 +6,25 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
+use Laravel\Cashier\Subscription;
 
 class WebhookController extends CashierController
 {
-	public function handleInvoicePaymentSucceeded($payload)
-	{
-		Log::debug(\GuzzleHttp\json_encode($payload));
-		return ['ok'];
-	}
+    public function handleInvoicePaymentSucceeded($payload)
+    {
+        Log::debug('handleInvoicePaymentSucceeded');
+        return ['ok'];
+    }
 
-	public function handleInvoiceUpcoming($payload)
-	{
-		Log::debug(\GuzzleHttp\json_encode($payload));
-		return ['ok'];
-	}
+    public function handleInvoiceUpcoming($payload)
+    {
+        Log::debug('handleInvoiceUpcoming');
+        return ['ok'];
+    }
 
-	public function handleInvoiceCreated($payload)
-	{
-		Log::debug(\GuzzleHttp\json_encode($payload));
-		return ['ok'];
-	}
+    public function handleInvoiceCreated($payload)
+    {
+        Log::debug('handleInvoiceCreated');
+        return ['ok'];
+    }
 }
