773dff59c08ec58ecd9f44155668de0fa49e4be7
Imad <imad.bazzal.93@gmail.com>
2017-12-23 09:00:03 +0200

    refactoring

--- resources/views/entity/article/partials/google-preview.blade.php
+++ resources/views/entity/article/partials/google-preview.blade.php
@@ -1,6 +1,5 @@
 <a href="https://docs.google.com/document/d/{{$article->google_id}}/edit">
     <strong>{{_i('See in Google Docs')}}</strong>
 </a>
-<embed src="{{url(str_replace('//', '', $article->export(\App\Services\Google\Drive::PDF)->getFullUrl()))}}"
-       width="100%"
-       height="450">
+
+<iframe src="https://docs.google.com/document/d/{{$article->google_id}}/edit"  width="100%" height="450" frameborder="0"></iframe>
