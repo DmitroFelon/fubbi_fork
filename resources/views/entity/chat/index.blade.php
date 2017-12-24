2a35351dd65444a73228a1ccd21cfdc89c555c01
Imad <imad.bazzal.93@gmail.com>
2017-12-24 19:38:55 +0200

    messages

--- resources/views/entity/chat/index.blade.php
+++ resources/views/entity/chat/index.blade.php
@@ -11,15 +11,61 @@
 @endsection
 
 @section('content')
-
-    @foreach($conversations as $conversation)
-
-        <div>
-            <a href="{{action('MessageController@show', $conversation->id)}}">conv</a>
+    <div class="tabs-container">
+        <div class="tabs-left">
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                <ul class="nav nav-tabs">
+                    @foreach($conversations as $conversation)
+                        <li class="{{($loop->first)?'active':''}}">
+                            <a class="message-switcher"
+                               data-sourse="{{action('MessageController@show', $conversation->id)}}"
+                               href="#">{{$conversation->data['title']}}</a>
+                        </li>
+                        <li>
+                            <a class="message-switcher"
+                               data-sourse="{{action('MessageController@show', $conversation->id)}}"
+                               href="#">{{$conversation->data['title']}}</a>
+                        </li>
+                        <li>
+                            <a class="message-switcher"
+                               data-sourse="{{action('MessageController@show', $conversation->id)}}"
+                               href="#">{{$conversation->data['title']}}</a>
+                        </li>
+                        <li>
+                            <a class="message-switcher"
+                               data-sourse="{{action('MessageController@show', $conversation->id)}}"
+                               href="#">{{$conversation->data['title']}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div id="messages-container" class="col-xs-11 col-sm-11 col-md-11 col-lg-11"></div>
         </div>
+    </div>
 
-    @endforeach
+@endsection
 
+@section('scripts')
+    <style>
+        .role-placeholder {
+            border-radius: 50%;
+            width: 1.5em;
+            height: 1.5em;
+            padding: 8px;
+            background: #fff;
+            border: 2px solid white;
+            color: white;
+            text-align: center;
+            font: 3em Arial, sans-serif;
+            line-height: 1em;
+            margin: 0;
+        }
+    </style>
+    <script>
+        $(".message-switcher").click(function () {
+            $("#messages-container").load($(this).attr('data-sourse'));
+        });
+    </script>
 @endsection
 
 
--- resources/views/entity/chat/show.blade.php
+++ resources/views/entity/chat/show.blade.php
@@ -1,66 +1,22 @@
-@extends('master')
+<div class="row">
 
-@section('before-content')
-    <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-lg-10">
-            <h2>
-                {{_i('Chat')}}
-            </h2>
+    {{ Form::open(['method' => 'POST', 'action' => 'MessageController@store' ]) }}
+    {{Form::hidden('conversation', $conversation)}}
+    <div class="col-md-10">
+        <div class="chat-discussion">
+            @each('entity.chat.partials.message', $chat_messages, 'chat_message')
         </div>
+        @include('entity.chat.partials.input')
     </div>
-@endsection
-
-@section('content')
-    <div class="wrapper wrapper-content animated fadeInRight">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="ibox chat-view">
-                    <div class="ibox-title">
-                        <small class="pull-right text-muted">Last message: Mon Jan 26 2015 - 18:39:23</small>
-                    </div>
-                    <div class="ibox-content">
-
-                        <div class="row">
-                            {{ Form::open(['method' => 'POST', 'action' => 'MessageController@store' ]) }}
-                            {{Form::hidden('conversation', $conversation)}}
-                            <div class="col-md-9">
-                                <div class="chat-discussion">
-                                    @each('entity.chat.partials.message', $chat_messages, 'chat_message')
-                                </div>
-                                @include('entity.chat.partials.input')
-                            </div>
-                            <div class="col-md-3">
-                                <div class="chat-users">
-                                    <div class="users-list">
-                                        @each('entity.chat.partials.participant', $participants, 'user')
-                                    </div>
-                                </div>
-                                <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
-                                    {{Form::submit('Send', ['class' => 'btn btn-primary btn-lg'])}}
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div>
+    <div class="col-md-2">
+        <div class="chat-users">
+            <div class="users-list">
+                @each('entity.chat.partials.participant', $participants, 'user')
             </div>
         </div>
-        <style>
-            .role-placeholder {
-                border-radius: 50%;
-                width: 1.5em;
-                height: 1.5em;
-                padding: 8px;
-                background: #fff;
-                border: 2px solid white;
-                color: white;
-                text-align: center;
-                font: 3em Arial, sans-serif;
-                line-height: 1em;
-                margin: 0;
-            }
-        </style>
-@endsection
-
-
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+            {{Form::submit('Send', ['class' => 'btn btn-primary btn-lg'])}}
+        </div>
+    </div>
+    {{Form::close()}}
+</div>
