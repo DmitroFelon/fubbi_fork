5590ea769d6217017b6ec4dbe29d3c51ceaa81f5
Imad <imad.bazzal.93@gmail.com>
2018-01-04 08:49:32 +0200

    refactoring

--- resources/views/entity/project/tabs/quiz.blade.php
+++ resources/views/entity/project/tabs/quiz.blade.php
@@ -5,7 +5,7 @@
 <div class="ibox-content">
 
     {!! Form::model($project,
-    ['files' => true, 'method' => 'PUT', 'role'=>'form', 'id' => 'quiz-form', 'action' => ['ProjectController@update', $project->id]])
+    ['files' => true, 'method' => 'POST', 'role'=>'form', 'id' => 'quiz-form', 'action' => ['ProjectController@update', $project->id]])
     !!}
     {!! Form::hidden('_step', \App\Models\Helpers\ProjectStates::QUIZ_FILLING) !!}
     {!! Form::hidden('_project_id', $project->id ) !!}
