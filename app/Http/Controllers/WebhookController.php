28b10f669e3c3896e0bcc00621f2131ce61d7a3c
Imad <imad.bazzal.93@gmail.com>
2018-01-12 13:04:38 +0200

    refactoring

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -6,25 +6,37 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
-use Laravel\Cashier\Subscription;
+
 
 class WebhookController extends CashierController
 {
     public function handleInvoicePaymentSucceeded($payload)
     {
         Log::debug('handleInvoicePaymentSucceeded');
-        return ['ok'];
+        return new Response('Webhook Handled', 200);
+    }
+
+    public function handleInvoicePaymentFailed($payload)
+    {
+        Log::debug('handleInvoicePaymentSucceeded');
+        return new Response('Webhook Handled', 200);
+    }
+
+    public function handleInvoicePaymentPending($payload)
+    {
+        Log::debug('handleInvoicePaymentSucceeded');
+        return new Response('Webhook Handled', 200);
     }
 
     public function handleInvoiceUpcoming($payload)
     {
         Log::debug('handleInvoiceUpcoming');
-        return ['ok'];
+        return new Response('Webhook Handled', 200);
     }
 
     public function handleInvoiceCreated($payload)
     {
         Log::debug('handleInvoiceCreated');
-        return ['ok'];
+        return new Response('Webhook Handled', 200);
     }
 }
