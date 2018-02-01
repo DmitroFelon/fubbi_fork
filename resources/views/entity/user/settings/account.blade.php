237cf00389b94c3cb779f6dc2f8b79063007e784
Imad <imad.bazzal.93@gmail.com>
2018-02-01 16:41:43 +0200

    refactoring

--- resources/views/entity/user/settings/account.blade.php
+++ resources/views/entity/user/settings/account.blade.php
@@ -22,57 +22,53 @@
 
 </div>
 
-@if($user->isWorker())
+<h3 class="text-center">{{_i('Address')}}</h3>
 
-    <h3 class="text-center">{{_i('Address')}}</h3>
+<div class="row">
 
-    <div class="row">
-
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            {!! Form::bsText('address_line_1', null, _i('Address Line 1'), null, [], 'text') !!}
-        </div>
-
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            {!! Form::bsText('address_line_2', null, _i('Address Line 2'), null, [], 'text') !!}
-        </div>
-
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        {!! Form::bsText('address_line_1', null, _i('Address Line 1'), null, [], 'text') !!}
     </div>
 
-    <div class="row">
-
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            {!! Form::bsText('zip', null, _i('Zip/Postal Code'), null, [], 'text') !!}
-        </div>
-
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            {!! Form::bsText('city', null, _i('City'), null, [], 'text') !!}
-        </div>
-
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        {!! Form::bsText('address_line_2', null, _i('Address Line 2'), null, [], 'text') !!}
     </div>
 
-    <div class="row">
+</div>
 
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            {!! Form::bsText('country', null, _i('Country'), null, [], 'text') !!}
-        </div>
+<div class="row">
 
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            {!! Form::bsText('state', null, _i('State/Province'), null, [], 'text') !!}
-        </div>
-
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        {!! Form::bsText('zip', null, _i('Zip/Postal Code'), null, [], 'text') !!}
     </div>
 
-    <h3 class="text-center">{{_i('Additional')}}</h3>
-
-    <div class="row">
-
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            {!! Form::bsText('how_did_you_find_us', null, _i('How Did You find us?'), null, [], 'text') !!}
-        </div>
-
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        {!! Form::bsText('city', null, _i('City'), null, [], 'text') !!}
     </div>
 
-@endif
+</div>
+
+<div class="row">
+
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        {!! Form::bsText('country', null, _i('Country'), null, [], 'text') !!}
+    </div>
+
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        {!! Form::bsText('state', null, _i('State/Province'), null, [], 'text') !!}
+    </div>
+
+</div>
+
+<h3 class="text-center">{{_i('Additional')}}</h3>
+
+<div class="row">
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        {!! Form::bsText('how_did_you_find_us', null, _i('How Did You find us?'), null, [], 'text') !!}
+    </div>
+
+</div>
 
 
 {{Form::submit(_i('Save'), ['class' => 'btn btn-primary'])}}
