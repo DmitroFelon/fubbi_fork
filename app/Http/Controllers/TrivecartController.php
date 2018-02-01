eb69459916a78961727a50ff28190ef1f58d1463
Imad <imad.bazzal.93@gmail.com>
2018-02-01 16:50:44 +0200

    refactoring

--- app/Http/Controllers/TrivecartController.php
+++ app/Http/Controllers/TrivecartController.php
@@ -15,38 +15,35 @@ class TrivecartController extends Controller
 {
     public function handle(Request $request)
     {
-        Log::debug($request->input());
-
         $event      = $request->input('event');
         $hash       = $request->input('thrivecart_secret'); // todo check on each request
         $product_id = $request->input('base_product'); // todo check on each request
 
 
         if ($event != 'order.success') {
+            Log::error('wrong event: ' . $event);
             return new Response('Webhook Handled', 200);
         }
 
         if (!$hash) {
+            Log::error('wrong hash: ' . $hash);
             return new Response('Webhook Handled', 200);
         }
 
-        if (!$product_id) {
+        if (!$product_id or !isset(config('fubbi.thrive_cart_plans')[$product_id])) {
+            Log::error('wrong product_id: ' . $product_id);
             return new Response('Webhook Handled', 200);
         }
-
 
         $customer_identifier = $request->input('customer_identifier'); //stripe customer id
         $customer            = $request->input('customer'); //form data
-        $subscriptions       = collect($request->input('subscriptions'))->first(); //subscription ids, get first
+        $subscription_id     = collect($request->input('subscriptions'))->first(); //subscription ids, get first
 
         $customer_email = $customer['email']??'';
 
-        $stripe_customer = Customer::retrieve($customer_identifier);
-
-        $stripe_subscription = Subscription::retrieve($subscriptions);
-
-        $custome_card = collect($stripe_customer->sources->data)->first();
-
+        $stripe_customer     = Customer::retrieve($customer_identifier);
+        $stripe_subscription = Subscription::retrieve($subscription_id);
+        $custome_card        = collect($stripe_customer->sources->data)->first();
 
         $user = User::whereEmail($customer_email)->get();
         if (!$user) {
@@ -62,18 +59,23 @@ class TrivecartController extends Controller
                 'card_brand'     => $custome_card->brand,
                 'card_last_four' => $custome_card->last4,
             ]);
-
             $user->setMeta('address_line_1', $custome_card->address_line1);
             $user->setMeta('zip', $custome_card->address_zip);
             $user->setMeta('city', $custome_card->address_city);
             $user->setMeta('country', $custome_card->address_country);
             $user->setMeta('state', $custome_card->address_state);
-
             $user->save();
-
             Cache::put($customer['ip_address'], $user->id, now()->addHour());
         }
 
+        $subscription              = new \Laravel\Cashier\Subscription;
+        $subscription->user_id     = $user->id;
+        $subscription->name        = str_random(10);
+        $subscription->stripe_id   = $subscription_id;
+        $subscription->stripe_plan = config('fubbi.thrive_cart_plans')[$product_id];
+        $subscription->quantity    = 1;
+
+        $subscription->save();
 
         return new Response('Webhook Handled', 200);
     }
