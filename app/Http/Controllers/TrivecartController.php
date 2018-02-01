2da727d17e05e527cfbac58a59ec2c8ac4c299c2
Imad <imad.bazzal.93@gmail.com>
2018-02-01 14:55:59 +0200

    refactoring

--- app/Http/Controllers/TrivecartController.php
+++ app/Http/Controllers/TrivecartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
@@ -10,21 +11,31 @@ class TrivecartController extends Controller
 {
     public function handle(Request $request)
     {
-        Log::debug('TrivecartController@handle');
+        Log::debug($request->input());
+        return new Response('Webhook Handled', 200);
+        $event = $request->input('event');
+        if (!'order.success' == $event) {
+            return new Response('Webhook Handled', 200);
+        }
 
-        $hash                = $request->input('thrivecart_secret');
-        $event               = $request->input('event');
-        $base_product        = $request->input('base_product');
-        $order_id            = $request->input('order_id');
-        $customer_id         = $request->input('customer_id');
+        $hash                = $request->input('thrivecart_secret'); // todo check on each request
         $customer_identifier = $request->input('customer_identifier'); //stripe customer id
         $customer            = $request->input('customer'); //form data
-        $order               = $request->input('order'); //form data
+        $order               = $request->input('order'); //array data
+        $subscriptions       = collect($request->input('subscriptions')); //subscription ids, get first
+        $purchases           = collect($request->input('purchases')); //product name to be validated
 
+        $customer_email = $customer['email']??'';
 
-        Log::debug($request->getContent());
+        $user = User::whereEmail($customer_email)->get();
+        if (!$user) {
+            $user = User::create([
+                'email'      => $customer_email,
+                'first_name' => $customer['first_name'] ?? '',
+                'last_name'  => $customer['last_name'] ?? '',
+            ]);
+        }
 
-        Log::debug($request->input());
 
         return new Response('Webhook Handled', 200);
     }
