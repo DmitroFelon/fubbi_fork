6677de308211eba438b258fec82ebe25c1399c24
Imad <imad.bazzal.93@gmail.com>
2018-02-01 11:16:39 +0200

    refactoring

--- app/Http/Controllers/TrivecartController.php
+++ app/Http/Controllers/TrivecartController.php
@@ -9,8 +9,20 @@ class TrivecartController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $hash                = $request->input('thrivecart_secret');
+        $event               = $request->input('event');
+        $base_product        = $request->input('base_product');
+        $order_id            = $request->input('order_id');
+        $customer_id         = $request->input('customer_id');
+        $customer_identifier = $request->input('customer_identifier'); //stripe customer id
+        $customer            = $request->input('customer'); //form data
+        $order               = $request->input('order'); //form data
+
+
         Log::debug($request->getContent());
 
-        return ['ok'];
+        Log::debug($request->input());
+
+
     }
 }
