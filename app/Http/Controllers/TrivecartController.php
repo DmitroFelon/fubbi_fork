87fd80b1f1a9362b0f98fb43c58d00b234f2bb02
Imad <imad.bazzal.93@gmail.com>
2018-02-01 17:03:58 +0200

    refactoring

--- app/Http/Controllers/TrivecartController.php
+++ app/Http/Controllers/TrivecartController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Helpers\ProjectStates;
+use App\Models\Project;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -35,47 +37,60 @@ class TrivecartController extends Controller
             return new Response('Webhook Handled', 200);
         }
 
-        $customer_identifier = $request->input('customer_identifier'); //stripe customer id
-        $customer            = $request->input('customer'); //form data
-        $subscription_id     = collect($request->input('subscriptions'))->first(); //subscription ids, get first
 
-        $customer_email = $customer['email']??'';
+        try {
+            $customer_identifier = $request->input('customer_identifier'); //stripe customer id
+            $customer            = $request->input('customer'); //form data
+            $subscription_id     = collect($request->input('subscriptions'))->first(); //subscription ids, get first
 
-        $stripe_customer     = Customer::retrieve($customer_identifier);
-        $stripe_subscription = Subscription::retrieve($subscription_id);
-        $custome_card        = collect($stripe_customer->sources->data)->first();
+            $customer_email = $customer['email']??'';
 
-        $user = User::whereEmail($customer_email)->get();
-        if (!$user) {
+            $stripe_customer     = Customer::retrieve($customer_identifier);
+            $stripe_subscription = Subscription::retrieve($subscription_id);
+            $custome_card        = collect($stripe_customer->sources->data)->first();
 
-            $user = User::create([
-                'email'          => $customer_email,
-                'first_name'     => $customer['first_name'] ?? '',
-                'last_name'      => $customer['last_name'] ?? '',
-                'password'       => Hash::make(str_random(8)),
-                'phone'          => $customer['contactno']??'',
-                'stripe_id'      => $stripe_subscription->customer,
-                'trial_ends_at'  => $stripe_subscription->trial_end,
-                'card_brand'     => $custome_card->brand,
-                'card_last_four' => $custome_card->last4,
-            ]);
-            $user->setMeta('address_line_1', $custome_card->address_line1);
-            $user->setMeta('zip', $custome_card->address_zip);
-            $user->setMeta('city', $custome_card->address_city);
-            $user->setMeta('country', $custome_card->address_country);
-            $user->setMeta('state', $custome_card->address_state);
-            $user->save();
-            Cache::put($customer['ip_address'], $user->id, now()->addHour());
+            $user = User::whereEmail($customer_email)->get();
+            if (!$user) {
+
+                $user = User::create([
+                    'email'          => $customer_email,
+                    'first_name'     => $customer['first_name'] ?? '',
+                    'last_name'      => $customer['last_name'] ?? '',
+                    'password'       => Hash::make(str_random(8)),
+                    'phone'          => $customer['contactno']??'',
+                    'stripe_id'      => $stripe_subscription->customer,
+                    'trial_ends_at'  => $stripe_subscription->trial_end,
+                    'card_brand'     => $custome_card->brand,
+                    'card_last_four' => $custome_card->last4,
+                ]);
+                $user->setMeta('address_line_1', $custome_card->address_line1);
+                $user->setMeta('zip', $custome_card->address_zip);
+                $user->setMeta('city', $custome_card->address_city);
+                $user->setMeta('country', $custome_card->address_country);
+                $user->setMeta('state', $custome_card->address_state);
+                $user->save();
+                Cache::put($customer['ip_address'], $user->id, now()->addHour());
+            }
+
+            $subscription              = new \Laravel\Cashier\Subscription;
+            $subscription->user_id     = $user->id;
+            $subscription->name        = str_random(10);
+            $subscription->stripe_id   = $subscription_id;
+            $subscription->stripe_plan = config('fubbi.thrive_cart_plans')[$product_id];
+            $subscription->quantity    = 1;
+
+            $subscription->save();
+
+            $project                  = new Project;
+            $project->client_id       = $user->id;
+            $project->subscription_id = $subscription->getKey();
+            $project->name            = 'Project #' . $user->projects()->count() + 1;
+            $project->state           = ProjectStates::QUIZ_FILLING;
+
+            $project->save();
+        } catch (\Exception $e) {
+            Log::error($e);
         }
-
-        $subscription              = new \Laravel\Cashier\Subscription;
-        $subscription->user_id     = $user->id;
-        $subscription->name        = str_random(10);
-        $subscription->stripe_id   = $subscription_id;
-        $subscription->stripe_plan = config('fubbi.thrive_cart_plans')[$product_id];
-        $subscription->quantity    = 1;
-
-        $subscription->save();
 
         return new Response('Webhook Handled', 200);
     }
