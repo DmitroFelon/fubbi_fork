a28e126c49be2de61782bda14f025f82d883a256
Imad <imad.bazzal.93@gmail.com>
2018-02-12 21:06:10 +0200

    refactoring

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('coockie/{key}/{value}', function (string $key, string $value) {
 
 Route::get('test', function (\Illuminate\Http\Request $request) {
     $server = $request->server();
-
+    
     if(isset($server['REQUEST_SCHEME']) and $server['REQUEST_SCHEME'] == 'http'){
         return redirect()->secure($request->getRequestUri());
     }
