d609d88a9904b1fd78bbf65226164fdb33b01833
Imad <imad.bazzal.93@gmail.com>
2018-02-20 11:19:04 +0200

    refactoring

--- resources/views/entity/user/settings.blade.php
+++ resources/views/entity/user/settings.blade.php
@@ -1,15 +1,5 @@
 @extends('master')
 
-@section('before-content')
-    <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-sm-4">
-            <h2>{{_i('Settings')}}</h2>
-        </div>
-    </div>
-@endsection
-
-
-
 @section('content')
 
     <div class="ibox">
